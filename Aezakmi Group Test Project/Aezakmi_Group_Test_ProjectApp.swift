1d7aaf82a4e956d746a40fd1882269451854333b
wwuselessww <sa24122@gmail.com>
2025-05-13 19:15:33 +0300

    Initial Commit

---
+++ Aezakmi Group Test Project/Aezakmi_Group_Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aezakmi_Group_Test_ProjectApp.swift
+//  Aezakmi Group Test Project
+//
+//  Created by Alexander Kozharin on 13.05.25.
+//
+
+import SwiftUI
+
+@main
+struct Aezakmi_Group_Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
