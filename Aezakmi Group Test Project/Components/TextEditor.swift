b1c0677de64fd8ce76b81b74706b9c4266d3832b
wwuselessww <sa24122@gmail.com>
2025-05-16 02:04:23 +0300

    added color and size and text entering in mainPage

---
+++ Aezakmi Group Test Project/Components/TextEditor.swift
@@ -0,0 +1,8 @@
+//
+//  TextEditor.swift
+//  Aezakmi Group Test Project
+//
+//  Created by Alexander Kozharin on 16.05.25.
+//
+
+import Foundation
---
+++ Aezakmi Group Test Project/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Extensions.swift
+//  Aezakmi Group Test Project
+//
+//  Created by Alexander Kozharin on 16.05.25.
+//
+
+import Foundation
