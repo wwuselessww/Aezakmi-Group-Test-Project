f738f5902bca8098d32a28f0c33a056e6e89e4ec
wwuselessww <sa24122@gmail.com>
2025-05-15 13:24:45 +0300

    added photo selection to mainPage

---
+++ Aezakmi Group Test Project/ViewModels/MainPageViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MainPageViewModel.swift
+//  Aezakmi Group Test Project
+//
+//  Created by Alexander Kozharin on 15.05.25.
+//
+
+import SwiftUI
+import PhotosUI
+
+class MainPageViewModel: ObservableObject {
+    @Published var selectedPhoto: PhotosPickerItem?
+    @Published var image: Image?
+}
--- Aezakmi Group Test Project/Views/MainPage.swift
+++ Aezakmi Group Test Project/Views/MainPage.swift
@@ -7,22 +7,51 @@
 
 import SwiftUI
 import FirebaseAuth
+import PhotosUI
 
 struct MainPage:View {
     @EnvironmentObject var appVM: AppViewModel
+    @StateObject var vm = MainPageViewModel()
+    
     var body: some View {
         VStack {
-            Text("\(Auth.auth().currentUser?.uid)")
-            Button {
-                appVM.signOut()
-            } label: {
-                Text("Sign out")
-                    .bold()
-                    .foregroundStyle(.white)
+//            Text("\(Auth.auth().currentUser?.uid)")
+//            Button {
+//                appVM.signOut()
+//            } label: {
+//                Text("Sign out")
+//                    .bold()
+//                    .foregroundStyle(.white)
+//            }
+//            .withDefaultButtonFormatting(disabled: .constant(false))
+//            .padding()
+            if let image = vm.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(minWidth: 50, maxWidth: 300, minHeight: 50, maxHeight: 300)
+            } else {
+                Image(systemName: "photo.artframe")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(minWidth: 50, maxWidth: 100)
             }
-            .withDefaultButtonFormatting(disabled: .constant(false))
-            .padding()
+            PhotosPicker("Select Photo For Edit", selection: $vm.selectedPhoto, matching: .images)
+                .foregroundStyle(.white)
+                .bold()
+                .withDefaultButtonFormatting(disabled: .constant(false))
 
+        }
+        .padding(.horizontal)
+        .onChange(of: vm.selectedPhoto) { oldValue, newValue in
+            Task {
+                do {
+                    let loaded = try await vm.selectedPhoto?.loadTransferable(type: Image.self)
+                        vm.image = loaded
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
